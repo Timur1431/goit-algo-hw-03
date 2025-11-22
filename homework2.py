235f0e15108fc76e11fb70faa59053897537d9b2
Timur <timurcyganczuk@gmail.com>
2025-11-22 19:10:41 +0100

    1

--- homework2.py
+++ homework2.py
@@ -3,10 +3,12 @@ import random
 def get_numbers_ticket(min, max, quantity):
     if not (1 <= min < max <= 1000):
         return []
-    if not (min <= quantity <= max - min + 1):
-        return []
     
     numbers = random.sample(range(min, max + 1), quantity)
 
 
     return sorted(numbers)
+
+for i in range (5):
+    print(get_numbers_ticket(10,20,1))
+    print(" ")
